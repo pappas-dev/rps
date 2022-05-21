282b19bfc47fca53b394d7863518b91f28c9593e
Lux-Sakura <luxsgraves@gmail.com>
2022-05-21 15:32:56 +0900

    send results to console.

---
+++ rps.js
@@ -0,0 +1,66 @@
+// The randomized RPS computer choices
+
+function computerPlay() {
+    let rps = Math.floor((Math.random() * 3) + 1);
+    if (rps === 1) {
+        return 'rock';
+    } if (rps === 2) {
+        return 'paper';
+    } if (rps === 3) {
+        return 'scissors';
+    } else {
+        return 'Something went wrong!'
+    }
+}
+
+// Make a round of RPS w/ result output.
+
+function playRound(playerSelection,computerSelection) {
+    playerSelection = prompt('Make your choice of rock, paper, or scissors'),'';
+    computerSelection = computerPlay();
+    if (computerSelection === 'rock' && playerSelection === 'rock'){
+        return `It's a rock tie!`;
+    }   if (computerSelection === 'paper' && playerSelection === 'rock'){
+        computerScore += 1;
+        return `The computer wins! Paper against rock!`;
+    }   if (computerSelection === 'scissors' && playerSelection === 'rock'){
+        playerScore += 1;
+        return `The player wins! Rock against scissors!`;
+    }   if (computerSelection === 'rock' && playerSelection === 'paper'){
+        playerScore += 1;
+        return`The player wins! Paper against rock!`;
+    }   if (computerSelection === 'paper' && playerSelection === 'paper'){
+        return `It's a paper tie!`;
+    }   if (computerSelection === 'scissors' && playerSelection === 'paper'){
+        computerScore += 1;
+        return`The computer wins! Scissors against paper!`;
+    }   if (computerSelection === 'rock' && playerSelection === 'scissors'){
+        computerScore += 1;
+        return `The computer wins! Rock against scissors.`;
+    }   if (computerSelection === 'paper' && playerSelection === 'scissors') {
+        playerScore += 1;
+        return `The player wins! Scissors against paper!`;
+    }   if (computerSelection === 'scissors' && playerSelection === 'scissors'){
+        return `It's a scissors tie!`;
+    }   else {
+        return alert('Must enter "rock", "paper", or "scissors" only.');
+    }
+}
+
+//Send results to console.
+
+let playerScore = 0;
+let computerScore = 0;
+let playerSelection;
+let computerSelection;
+function game() {
+for (let i = 1; i <= 5; i++) {
+    console.log(playRound(playerSelection,computerSelection));
+    console.log(`Computer: ${computerScore} ; Player: ${playerScore}`);
+}
+if (computerScore > playerScore) {
+    console.log('Computer wins out of 5 games.');
+}   else {
+        console.log('Player wins out of 5 games!');
+    }
+}
