d64077a3d3ce2f719cf5946453934c1d8ba35693
Lux-Sakura <luxsgraves@gmail.com>
2022-05-29 21:06:00 +0900

    add GUI to RPS

--- rps.js
+++ rps.js
@@ -13,52 +13,111 @@ function computerPlay() {
     }
 }
 
-// Make a round of RPS w/ result output.
+// set up 3 functions for each button as a choice.
 
-function playRound(playerSelection,computerSelection) {
-    playerSelection = prompt('Make your choice of rock, paper, or scissors'),'';
+function playRock(playerSelection,computerSelection) {
+    playerSelection = 'rock';
     computerSelection = computerPlay();
     if (computerSelection === 'rock' && playerSelection === 'rock'){
-        computerScore += 1;
-        playerScore += 1;
-        return `It's a rock tie!`;
+        return;
     }   if (computerSelection === 'paper' && playerSelection === 'rock'){
         computerScore += 1;
-        return `The computer wins! Paper against rock!`;
+        computerScorePrint.textContent = `${computerScore}`;
+        return;
     }   if (computerSelection === 'scissors' && playerSelection === 'rock'){
         playerScore += 1;
-        return `The player wins! Rock against scissors!`;
-    }   if (computerSelection === 'rock' && playerSelection === 'paper'){
-        playerScore += 1;
-        return`The player wins! Paper against rock!`;
-    }   if (computerSelection === 'paper' && playerSelection === 'paper'){
-        computerScore += 1;
-        playerScore += 1;
-        return `It's a paper tie!`;
-    }   if (computerSelection === 'scissors' && playerSelection === 'paper'){
-        computerScore += 1;
-        return`The computer wins! Scissors against paper!`;
-    }   if (computerSelection === 'rock' && playerSelection === 'scissors'){
-        computerScore += 1;
-        return `The computer wins! Rock against scissors.`;
-    }   if (computerSelection === 'paper' && playerSelection === 'scissors') {
-        playerScore += 1;
-        return `The player wins! Scissors against paper!`;
-    }   if (computerSelection === 'scissors' && playerSelection === 'scissors'){
-        computerScore += 1;
-        playerScore += 1;
-        return `It's a scissors tie!`;
-    }   else {
-        return alert('Must enter "rock", "paper", or "scissors" only.');
+        playerScorePrint.textContent = `${playerScore}`;
+        return;
+    }   if (pScore === 5) {
+        location.reload();
+    }   if (cScore === 5) {
+        location.reload();
     }
 }
 
-//Send results to alerts and webpage for final results.
+function playPaper(playerSelection,computerSelection) {
+    playerSelection = 'paper';
+    computerSelection = computerPlay();
+       if (computerSelection === 'rock' && playerSelection === 'paper'){
+        playerScore += 1;
+        playerScorePrint.textContent = `${playerScore}`;
+        return;
+    }   if (computerSelection === 'paper' && playerSelection === 'paper'){
+        return;
+    }   if (computerSelection === 'scissors' && playerSelection === 'paper'){
+        computerScore += 1;
+        computerScorePrint.textContent = `${computerScore}`;
+        return;
+    }   if (pScore === 5) {
+        location.reload();
+    }   if (cScore === 5) {
+        location.reload();
+    }
+}
+
+function playScissors(playerSelection,computerSelection) {
+    playerSelection = 'scissors';
+    computerSelection = computerPlay();
+        if (computerSelection === 'rock' && playerSelection === 'scissors'){
+        computerScore += 1;
+        computerScorePrint.textContent = `${computerScore}`;
+        return;
+    }   if (computerSelection === 'paper' && playerSelection === 'scissors') {
+        playerScore += 1;
+        playerScorePrint.textContent = `${playerScore}`;
+        return;
+    }   if (computerSelection === 'scissors' && playerSelection === 'scissors'){
+        return;
+    }   if (pScore === 5) {
+        location.reload();
+    }   if (cScore === 5) {
+        location.reload();
+    }
+}
+
+//set variables below
 
 let playerScore = 0;
 let computerScore = 0;
 let playerSelection;
 let computerSelection;
+
+//map functions to buttons below
+
+let rockButton = document.querySelector('.rockB');
+rockButton.addEventListener('click',playRock);
+
+let paperButton = document.querySelector('.paperB');
+paperButton.addEventListener('click',playPaper);
+
+let scissorsButton = document.querySelector('.scissorsB');
+scissorsButton.addEventListener('click',playScissors);
+
+let playerScorePrint = document.querySelector('.pScore');
+let computerScorePrint = document.querySelector('.cScore');
+
+let pScore = playerScorePrint.textContent = `${playerScore}`;
+let cScore = computerScorePrint.textContent = `${computerScore}`;
+
+/*
+function compWins () {
+    if (computerScorePrint === 5) {
+        alert(`The computer wins!`);
+        location.reload();
+        return;
+    } if (playerScorePrint === 5) {
+        alert(`The computer wins!`);
+        location.reload();
+        return;
+    } if (playerScorePrint === 5 && computerScorePrint === 5) {
+        alert(`It's a tie!`);
+        location.reload;
+        return;
+    }
+}
+compWins();
+
+
 function game() {
 for (let i = 1; i <= 5; i++) {
     alert(playRound(playerSelection,computerSelection));
@@ -72,3 +131,4 @@ if (computerScore > playerScore) {
         location.replace('player.html');
     }
 }
+*/
