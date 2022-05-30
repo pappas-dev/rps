8365ac83de5a70be56ef5ecc01d8f0cb757e4218
Lux-Sakura <luxsgraves@gmail.com>
2022-05-31 00:35:26 +0900

    integrate score functionality.

--- rps.js
+++ rps.js
@@ -30,16 +30,14 @@ function playRock(playerSelection,computerSelection) {
         playerScore += 1;
         playerScorePrint.textContent = `${playerScore}`;
         computerScorePrint.textContent = `${computerScore}`;
-        return playerScore, computerScore;
     }   if (computerSelection === 'paper' && playerSelection === 'rock'){
         computerScore += 1;
         computerScorePrint.textContent = `${computerScore}`;
-        return computerScore;
     }   if (computerSelection === 'scissors' && playerSelection === 'rock'){
         playerScore += 1;
         playerScorePrint.textContent = `${playerScore}`;
-        return playerScore;
-    }
+    }   gameOver();
+        return;
 }
 
 function playPaper(playerSelection,computerSelection) {
@@ -48,18 +46,16 @@ function playPaper(playerSelection,computerSelection) {
        if (computerSelection === 'rock' && playerSelection === 'paper'){
         playerScore += 1;
         playerScorePrint.textContent = `${playerScore}`;
-        return playerScore;
     }   if (computerSelection === 'paper' && playerSelection === 'paper'){
         computerScore += 1;
         playerScore += 1;
         playerScorePrint.textContent = `${playerScore}`;
         computerScorePrint.textContent = `${computerScore}`;
-        return playerScore, computerScore;
     }   if (computerSelection === 'scissors' && playerSelection === 'paper'){
         computerScore += 1;
         computerScorePrint.textContent = `${computerScore}`;
-        return computerScore;
-    }
+    }   gameOver();
+        return;
 }
 
 function playScissors(playerSelection,computerSelection) {
@@ -68,27 +64,44 @@ function playScissors(playerSelection,computerSelection) {
         if (computerSelection === 'rock' && playerSelection === 'scissors'){
         computerScore += 1;
         computerScorePrint.textContent = `${computerScore}`;
-        return computerScore;
     }   if (computerSelection === 'paper' && playerSelection === 'scissors') {
         playerScore += 1;
         playerScorePrint.textContent = `${playerScore}`;
-        return playerScore;
     }   if (computerSelection === 'scissors' && playerSelection === 'scissors'){
         computerScore += 1;
         playerScore += 1;
         playerScorePrint.textContent = `${playerScore}`;
         computerScorePrint.textContent = `${computerScore}`;
-        return playerScore, computerScore;
+    }   gameOver();
+        return;
+} 
+
+
+// kill buttons with score of 5 and play again button.
+
+function gameOver() {
+    if (playerScore === 5) {
+        rockButton.disabled = true;
+        paperButton.disabled = true;
+        scissorsButton.disabled = true;
+        playerPrint.textContent = `You won!`;
+    } if (computerScore === 5) {
+        rockButton.disabled = true;
+        paperButton.disabled = true;
+        scissorsButton.disabled = true;
+        computerPrint.textContent = `The computer won!`;
+    } if (playerScore === 5 && computerScore === 5) {
+        rockButton.disabled = true;
+        paperButton.disabled = true;
+        scissorsButton.disabled = true;
+        playerPrint.textContent = `It was a tie!`;
+        computerPrint.textContent = `It was a tie!`;  
     }
+ }
+// new game.
+function playAgain() {
+    location.reload();
 }
-
-// kill buttons with score of 5. not implemented yet.
-
-function disableButtons() {
-    rockButton.disabled = true;
-    paperButton.disabled = true;
-    scissorsButton.disabled = true;
-    }
 
 //map functions to buttons below
 
@@ -101,5 +114,11 @@ paperButton.addEventListener('click',playPaper);
 let scissorsButton = document.querySelector('.scissorsB');
 scissorsButton.addEventListener('click',playScissors);
 
+let playBtn = document.querySelector('.play');
+playBtn.addEventListener('click',playAgain);
+
 let playerScorePrint = document.querySelector('.pScore');
 let computerScorePrint = document.querySelector('.cScore');
+
+let playerPrint = document.querySelector('.pText');
+let computerPrint = document.querySelector('.cText');
